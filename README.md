75c4888c2626e7ffed84da2cd96538633f0f9718
tmiyamon <miyamototakuya@gmail.com>
2015-09-07 09:00:16 +0900

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sample project for gradle-mdicons plugin
+
+A sample project for [gradle-mdicons plugin](https://github.com/tmiyamon/gradle-mdicons)
---
+++ app/src/main/java/com/tmiyamon/sample/MainActivity.java
@@ -0,0 +1,60 @@
+package com.tmiyamon.sample;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import java.lang.reflect.Field;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        ViewGroup view = (ViewGroup)findViewById(R.id.ll_main);
+
+        Field[] fields = R.mipmap.class.getFields();
+        for (Field f : fields) {
+            try {
+                int mipmapId = f.getInt(null);
+                ImageView imageView = new ImageView(this);
+                imageView.setImageResource(mipmapId);
+                imageView.setLayoutParams(new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT));
+
+                view.addView(imageView);
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
